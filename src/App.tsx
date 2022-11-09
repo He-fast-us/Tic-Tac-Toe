7c6142072f286688df68a94eb56403f0756e2687
Noah Hallett <113746129+He-fast-us@users.noreply.github.com>
2022-11-09 14:36:02 -0600

    Final working version with mobile responsive grid

--- src/App.tsx
+++ src/App.tsx
@@ -26,22 +26,16 @@ function Board(props: BoardProps) {
     />
 
   return (
-    <div>
-      <div className="board-row">
+    <div className="board">
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
-      </div>
-      <div className="board-row">
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
-      </div>
-      <div className="board-row">
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
-      </div>
     </div>
   );
 }
